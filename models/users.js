f6bf31e56ccb8a175d2f887b0ce6fbcb829f79dd
Nicholas Wells <n.wells3@icloud.com>
2018-09-06 21:04:41 -0400

    changes

--- models/users.js
+++ models/users.js
@@ -1,4 +1,4 @@
-  // *G)* Navigate to the `models` folder and create a new file called `todo.js`. Create a *Todo* model with columns for _"text"_ `DataTypes.STRING`, and _"complete"_ `DataTypes.BOOLEAN`.
+  
 
 
 module.exports = function (sequelize, DataTypes) {
